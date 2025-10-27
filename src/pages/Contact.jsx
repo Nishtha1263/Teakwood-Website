0ae8dfa17aa6e2158976436140858fcae158beb9
Nishtha1263 <nishthaagarwaln@gmail.com>
2025-10-27 18:03:21 +0530

    home changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,49 @@
 import React from "react";
-import HomeCarousel from "../components/Home/Hero";
-import Welcome from "../components/Home/Welcome";
-import Amenities from "../components/Amenities/Amenities";
-import Activities from "../components/Home/Activities";
-import Stay from "../components/Home/Stay";
-import Packages from "../components/Home/Packages";
-import Reviews from "../components/Home/Reviews";
-import Contact from "../components/Home/Contact";
+import "./ContactInfo.css";
 
-const HomePage = () => {
+const ContactInfo = () => {
   return (
-    <div className="homepage">
-      {/* <HomeCarousel />
-      <Welcome />
-      <Amenities />
-      <Activities />
-      <Stay />
-      <Packages />
-      <Reviews /> */}
-      <Contact />
-    </div>
+    <section className="contact-section">
+      <div className="contact-overlay"></div>
+
+      <div className="contact-container">
+        <div className="contact-text">
+          <h2>Contact Us</h2>
+          <p>We’d love to hear from you! Reach us through any of the options below or fill out the form to plan your stay.</p>
+
+          <div className="contact-details">
+            <a href="tel:+919881301029" className="contact-link">
+              +91 98813 01029
+            </a>
+            <a href="mailto:teakwoodcamping@gmail.com" className="contact-link">
+              teakwoodcamping@gmail.com
+            </a>
+            <a
+              href="https://maps.app.goo.gl/eJBkMc1jBSdBnjLg7"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contact-link"
+            >
+              S. No. 38, Malegaon-Salawade Road, Bhor, Pune, Maharashtra 412213
+            </a>
+          </div>
+        </div>
+
+        <div className="contact-form full-form">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdrWrzuKbHzpGBTaeV7awXGBlRDoLfZr3cs9X8uBBlZOBdblQ/viewform?embedded=true"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+          >
+            Loading…
+          </iframe>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default HomePage;
+export default ContactInfo;
